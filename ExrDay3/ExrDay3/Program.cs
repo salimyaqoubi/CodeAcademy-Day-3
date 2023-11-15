efbd2c4fd9711539b2e2eb7e3d33fccef976fe41
salimyaqoubi <82412701+salimyaqoubi@users.noreply.github.com>
2023-11-15 20:09:49 +0400

    Add files via upload

---
+++ ExrDay3/ExrDay3/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ExrDay2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x = 9; int y = 32;
+            string result = (x % 3 == 0) && (y % 3 == 0) ? "Divisible" : "NotDivisible";
+            Console.WriteLine(result);
+
+            string result2 = (x % 3 == 0) || (3 % 2 == 0) ? "Divisible" : "NotDivisible";
+            Console.WriteLine(result2);
+
+            //Console.WriteLine("Enter Number :");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num % 3 == 0)
+            //Console.WriteLine("Divisible");
+            // else
+            //  Console.WriteLine("Not Divisible");
+            
+            //int x = 3;
+            //Console.WriteLine(x++);
+            // Console.WriteLine(x);
+            */
+
+
+            int x = 1;
+            int y = 2;
+            Console.WriteLine(x++ + y);
+
+
+        }
+    }
+}
